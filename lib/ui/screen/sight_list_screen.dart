d4983547b3f8a711e781bc44fcc7645926f1907e
ShoshinAS <ShoshinAS@protonmail.com>
2022-03-30 23:45:54 +0400

    задание 4.4 (2)

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -15,25 +15,61 @@ class _SightListScreenState extends State<SightListScreen> {
     return Scaffold(
       backgroundColor: widget._BackgroundColor,
       appBar: AppBar(
-        title: const Padding(
-          padding: EdgeInsets.fromLTRB(16, 40, 16, 0),
-          child: Text(
-              'Список\nинтересных мест',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                color: Color(0XFF3B3E5B),
-                fontSize: 32,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.bold,
-              ),
+        title: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
+          child: RichText(
+            text: const TextSpan(
+              children: [
+                TextSpan(
+                  text: 'С',
+                  style: TextStyle(
+                    color: Color(0XFF4CAF50),
+                    fontSize: 32,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.bold,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'писок\n',
+                      style: TextStyle(
+                        color: Color(0XFF3B3E5B),
+                        fontSize: 32,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                TextSpan(
+                  text: 'и',
+                  style: TextStyle(
+                    color: Color(0XFFFCDD3D),
+                    fontSize: 32,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.bold,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'нтересных мест',
+                      style: TextStyle(
+                        color: Color(0XFF3B3E5B),
+                        fontSize: 32,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
+            textAlign: TextAlign.left,
           ),
+        ),
         backgroundColor: widget._BackgroundColor,
         elevation: 0,
         toolbarHeight: 112,
       ),
-      body: const Center(
-      ),
+      body: const Center(),
       resizeToAvoidBottomInset: false,
     );
   }
