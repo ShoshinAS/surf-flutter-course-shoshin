b525071b091693d2b52039ec5aba714cbb08bc9f
ShoshinAS <ShoshinAS@protonmail.com>
2022-03-30 18:06:38 +0400

    задание 4.4

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SightListScreen extends StatefulWidget {
+  final _BackgroundColor = Colors.white;
+
   const SightListScreen({Key? key}) : super(key: key);
 
   @override
@@ -10,9 +12,27 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello!'),
+    return Scaffold(
+      backgroundColor: widget._BackgroundColor,
+      appBar: AppBar(
+        title: const Padding(
+          padding: EdgeInsets.fromLTRB(16, 40, 16, 0),
+          child: Text(
+              'Список\nинтересных мест',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: Color(0XFF3B3E5B),
+                fontSize: 32,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        backgroundColor: widget._BackgroundColor,
+        elevation: 0,
+        toolbarHeight: 112,
+      ),
+      body: const Center(
       ),
       resizeToAvoidBottomInset: false,
     );
