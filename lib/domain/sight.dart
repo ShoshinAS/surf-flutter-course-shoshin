216e7cce3bacf343ab22b1f82917e58abe54fc43
ShoshinAS <ShoshinAS@protonmail.com>
2022-04-01 22:44:40 +0400

    задание 4.6 (1)

---
+++ lib/domain/sight.dart
@@ -0,0 +1,17 @@
+class Sight{
+  String name = '';
+  double lat = 0.0;
+  double lon = 0.0;
+  String url = '';
+  String details = '';
+  int type = 0;
+
+  Sight({
+    required this.name,
+    required this.lat,
+    required this.lon,
+    required this.url,
+    required this.details,
+    required this.type
+});
+}
